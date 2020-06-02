d45e10415c389111ab81d2fd8ec7f6c3b3789027
joselc20015 <joselc20015@gmail.com>
2020-06-01 23:35:02 -0300

    hello word

---
+++ projet/target/debug/deps/projet-d79ce33fc8df40fa.d
@@ -0,0 +1,5 @@
+/home/c/Rust/projet/target/debug/deps/projet-d79ce33fc8df40fa: src/main.rs
+
+/home/c/Rust/projet/target/debug/deps/projet-d79ce33fc8df40fa.d: src/main.rs
+
+src/main.rs:
---
+++ projet/target/debug/projet.d
@@ -0,0 +1,1 @@
+/home/c/Rust/projet/target/debug/projet: /home/c/Rust/projet/src/main.rs
